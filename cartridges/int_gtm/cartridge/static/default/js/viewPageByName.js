7e8d11139db9e9dbd35ecbfaac4d58a6322ecb5c
AzureAD\MeghaNair(G10XIND) <Megha.Nair@g10x.com>
2024-07-03 11:27:17 +0530

    Product View Page

---
+++ cartridges/int_gtm/cartridge/static/default/js/viewPageByName.js
@@ -0,0 +1,23 @@
+window.dataLayer = window.dataLayer || [];
+console.log("test");
+ 
+document.addEventListener('DOMContentLoaded', function () {
+    var productLinks = document.querySelectorAll('.pdp-link > a');
+ 
+    productLinks.forEach(function (element) {
+        element.addEventListener('click', function () {
+            var productName = element.textContent.trim();
+ 
+            console.log("Product Name:", productName);
+           
+            var product = {
+                name: productName,
+            };
+           
+            dataLayer.push({
+                'event': 'product_link_click',
+                'Productname': product.name,
+            });
+        });
+    });
+});

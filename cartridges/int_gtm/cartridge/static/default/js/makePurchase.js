9829bf9040e083318e548cd31e4aa8ca398f8684
AzureAD\NehaRachelSamuel(G10 <Neha.Samuel@g10x.com>
2024-07-03 12:46:46 +0530

    added more product and shipping details

--- cartridges/int_gtm/cartridge/static/default/js/makePurchase.js
+++ cartridges/int_gtm/cartridge/static/default/js/makePurchase.js
@@ -6,19 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
     submitPaymentButton.forEach(function (button) {
         button.addEventListener('click', function () {
             var grandTotalPriceElement = document.querySelector('.grand-total-price');
+            var orderDiscountPrice = document.querySelector('.order-discount-total');
+            var shippingTtotalCost = document.querySelector('.shipping-total-cost');
             var grandTotalPrice = grandTotalPriceElement ? grandTotalPriceElement.textContent.trim() : '';
+            var orderDiscount = orderDiscountPrice ? orderDiscountPrice.textContent.trim() : '';
+            var shippingCost = shippingTtotalCost ? shippingTtotalCost.textContent.trim() : '';
+            var productNameElements = document.getElementsByClassName('line-item-name');
+            var productNames = [];
+            for (var i = 0; i < productNameElements.length; i++) {
+                var productName = productNameElements[i].querySelector('span').textContent.trim();
+                console.log(productName);
+                productNames.push(productName);
+            }
+            var purchaseData = {
+                totalPrice: grandTotalPrice,
+                discount:orderDiscount,
+                shippingPrice:shippingCost,
+                products:productNames
 
-            var test = {
-                testvalue: grandTotalPrice
             };
 
             dataLayer.push({
                 'event': 'make_purchase',
-                'grandTotalPrice': test.testvalue
+                'grandTotalPrice': purchaseData.totalPrice,
+                'orderDiscountPrice':purchaseData.discount,
+                'shippingCost':purchaseData.shippingPrice,
+                'productNames':purchaseData.products
+
             });
 
             // Debugging
             console.log('Grand Total Price:', grandTotalPrice);
+ 
+
         });
     });
 });

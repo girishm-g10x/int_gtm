1fb2342eb08b5be20d4798935d810aeba92e96a6
AzureAD\SandraAnnShibu(G10XI <Sandra.Shibu@g10x.com>
2024-07-03 16:00:38 +0530

    remove from cart done

--- cartridges/int_gtm/cartridge/static/default/js/dataLayerUtil.js
+++ cartridges/int_gtm/cartridge/static/default/js/dataLayerUtil.js
@@ -1,23 +1,35 @@
 'use strict';
 
 window.dataLayer = window.dataLayer || [];
-
 document.addEventListener('DOMContentLoaded', function () {
-    var removeFromCartButtons = document.querySelectorAll('.cart-delete-confirmation-btn');
-   
+    var removeFromCartButtons = document.querySelectorAll(
+        '.cart-delete-confirmation-btn'
+    );
     removeFromCartButtons.forEach(function (button) {
         button.addEventListener('click', function () {
-            var productNameElement = button.closest('.card').querySelector('.product-name');
-            var productName = productNameElement ? productNameElement.textContent.trim() : '';
+            // Use jQuery to access data attributes set on the button
+            var $button = $(this);
+            var productId = $button.data('pid'); // Accessing data-pid
+            var actionUrl = $button.data('action'); // Accessing data-action
+            var uuid = $button.data('uuid'); // Accessing data-uuid
 
-            var productId = button.getAttribute('data-product-id');
+            var modal = this.closest('.modal-content');
 
-            // Push data to dataLayer
-            dataLayer.push({
-                'event': 'removeFromCart',
-                'productName': productName,
-                'productId': productId
-            });
+            if (modal) {
+                // jQuery to find product name element inside modal
+                var productNameElement = $(modal).find('.product-to-remove');
+                var productName = productNameElement.length
+                    ? productNameElement.text().trim()
+                    : '';
+
+                // Push data to dataLayer
+                window.dataLayer.push({
+                    event: 'removeFromCart',
+                    productName: productName,
+                    productId: productId,
+                    uuid: uuid
+                });
+            }
         });
     });
 });

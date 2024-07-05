11a1cd98a53a0719722a8a6af59448c274e43b5b
AzureAD\AromalDhananjayan(G1 <Aromal.Dhananjayan@g10x.com>
2024-07-05 17:21:25 +0530

    commented fixes

--- cartridges/int_gtm/cartridge/controllers/gtmIdSet.js
+++ cartridges/int_gtm/cartridge/controllers/gtmIdSet.js
@@ -5,30 +5,38 @@
  * This is a controller to set the gtm id given from the BM
  */
 
+/**
+ * gtmIdSet-SetBody : This end point is to set the body tag 
+ * @name gtmIdSet-SetBody
+ * @function
+ * @param {renders} - isml
+ */
 var server = require('server');
 
-server.get('SetBody',function (req, res, next) {
-
+server.get('SetBody', function (req, res, next) {
     var Site = require('dw/system/Site');
     var gtmContainerId = Site.current.getCustomPreferenceValue('GTMID') || '';
 
-    res.render('/tagManager/tagBody',{
+    res.render('/tagManager/tagBody', {
         id: gtmContainerId
-        });
-    
-next();
+    });
+    next();
 });
 
-server.get('SetHead',function (req, res, next) {
-
+/**
+ * gtmIdSet-SetHead : This end point is to set the Head tag 
+ * @name gtmIdSet-SetHead
+ * @function
+ * @param {renders} - isml
+ */
+server.get('SetHead', function (req, res, next) {
     var Site = require('dw/system/Site');
     var gtmContainerId = Site.current.getCustomPreferenceValue('GTMID') || '';
-    
-    res.render('/tagManager/tagHead',{
+
+    res.render('/tagManager/tagHead', {
         id: gtmContainerId
-        });
-    
-next();
+    });
+    next();
 });
 
 module.exports = server.exports();
--- cartridges/int_gtm/cartridge/static/default/js/TagManager.js
+++ cartridges/int_gtm/cartridge/static/default/js/TagManager.js
@@ -1,78 +1,80 @@
 window.dataLayer = window.dataLayer || [];
 
 document.addEventListener('DOMContentLoaded', function () {
+    function getProductDetails() {
+        var productNameElement = document.querySelector('.product-name');
+        var productName = productNameElement
+            ? productNameElement.textContent.trim()
+            : '';
 
-      function getProductDetails() {
-          var productNameElement = document.querySelector('.product-name');
-          var productName = productNameElement
-              ? productNameElement.textContent.trim()
-              : '';
+        var productContainer = document.querySelector(
+            '.product-detail.product-wrapper'
+        );
+        var productId = productContainer
+            ? productContainer.getAttribute('data-pid')
+            : '';
 
-          var productContainer = document.querySelector(
-              '.product-detail.product-wrapper'
-          );
-          var productId = productContainer
-              ? productContainer.getAttribute('data-pid')
-              : '';
+        var priceElement = document.querySelector('.prices');
+        var productPriceText = priceElement
+            ? priceElement.textContent.trim()
+            : '';
+        var regex = /£[\d,]+\.\d{2}/;
+        var match = productPriceText.match(regex);
+        var productPrice = match ? match[0] : '';
 
-          var priceElement = document.querySelector('.prices');
-          var productPriceText = priceElement
-              ? priceElement.textContent.trim()
-              : '';
-          var regex = /£[\d,]+\.\d{2}/;
-          var match = productPriceText.match(regex);
-          var productPrice = match ? match[0] : '';
+        var product = {
+            name: productName,
+            id: productId,
+            price: productPrice
+        };
 
-          var product = {
-              name: productName,
-              id: productId,
-              price: productPrice
-          };
+        dataLayer.push({
+            event: 'p-d-p-page',
+            Productname: product.name,
+            Productprice: productPrice,
+            Productid: product.id
+        });
+    }
 
-          dataLayer.push({
-              event: 'p-d-p-page',
-              Productname: product.name,
-              Productprice: productPrice,
-              Productid: product.id
-          });
-      }
-  
-     var removeFromCartButtons = document.querySelectorAll('.cart-delete-confirmation-btn');
-      removeFromCartButtons.forEach(function (button) {
+    var removeFromCartButtons = document.querySelectorAll(
+        '.cart-delete-confirmation-btn'
+    );
+    removeFromCartButtons.forEach(function (button) {
         button.addEventListener('click', function () {
- 
             // Use jQuery to access data attributes set on the button
             var $button = $(this);
             var productId = $button.data('pid'); // Accessing data-pid
             var actionUrl = $button.data('action'); // Accessing data-action
             var uuid = $button.data('uuid'); // Accessing data-uuid
- 
+
             var modal = this.closest('.modal-content');
- 
+
             if (modal) {
-               
                 // jQuery to find product name element inside modal
                 var productNameElement = $(modal).find('.product-to-remove');
-                var productName = productNameElement.length ? productNameElement.text().trim() : '';
- 
+                var productName = productNameElement.length
+                    ? productNameElement.text().trim()
+                    : '';
+
                 // Push data to dataLayer
                 window.dataLayer.push({
-                    'event': 'remove_from_cart',
-                    'productName': productName,
-                    'productId': productId,
-                    'uuid': uuid
+                    event: 'remove_from_cart',
+                    productName: productName,
+                    productId: productId,
+                    uuid: uuid
                 });
             }
         });
-       
+    });
+
     var promoForm = document.querySelector('.promo-code-form');
- 
+
     promoForm.addEventListener('submit', function (event) {
         event.preventDefault();
- 
+
         var couponCodeField = document.querySelector('.coupon-code-field');
         var couponCode = couponCodeField.value;
- 
+
         if (couponCode.trim() === '') {
             document.getElementById('missingCouponCode').style.display =
                 'block';
@@ -80,41 +82,49 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             document.getElementById('missingCouponCode').style.display = 'none';
         }
- 
+
         dataLayer.push({
             event: 'couponcheck',
             couponid: couponCode
         });
     });
-  
+
     function handleCheckoutEvent() {
-        var productNameElements = document.getElementsByClassName('line-item-name');
-        
-        for (var i = 0; i < (productNameElements.length/2); i++) {
-            var productName = productNameElements[i].querySelector('span').textContent.trim();
-            
+        var productNameElements =
+            document.getElementsByClassName('line-item-name');
+
+        for (var i = 0; i < productNameElements.length / 2; i++) {
+            var productName = productNameElements[i]
+                .querySelector('span')
+                .textContent.trim();
+
             var product = {
-                name: productName 
+                name: productName
             };
-            
+
             dataLayer.push({
-                'event': 'checkout',
-                'Product_Name': product.name
+                event: 'checkout',
+                Product_Name: product.name
             });
         }
     }
 
     var observer = new MutationObserver(function (mutationsList) {
         for (var mutation of mutationsList) {
-            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-                
-                var targetNodeForCheckout = mutation.target.querySelector('.container.data-checkout-stage');
-              
-                var targetNodeForPdp = mutation.target.querySelector('.product-wrapper');
-              
+            if (
+                mutation.type === 'childList' &&
+                mutation.addedNodes.length > 0
+            ) {
+                var targetNodeForCheckout = mutation.target.querySelector(
+                    '.container.data-checkout-stage'
+                );
+
+                var targetNodeForPdp =
+                    mutation.target.querySelector('.product-wrapper');
+
                 if (targetNodeForCheckout) {
                     handleCheckoutEvent();
-                    break; 
+                    break;
                 }
                 if (targetNodeForPdp) {
                     getProductDetails();
@@ -127,41 +137,88 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(document.body, {
         childList: true,
         subtree: true
-    });  
+    });
 
     var submitPaymentButton = document.querySelectorAll('.place-order');
 
     submitPaymentButton.forEach(function (button) {
         button.addEventListener('click', function () {
-            var grandTotalPriceElement = document.querySelector('.grand-total-price');
-            var orderDiscountPrice = document.querySelector('.order-discount-total');
-            var shippingTtotalCost = document.querySelector('.shipping-total-cost');
-            var grandTotalPrice = grandTotalPriceElement ? grandTotalPriceElement.textContent.trim() : '';
-            var orderDiscount = orderDiscountPrice ? orderDiscountPrice.textContent.trim() : '';
-            var shippingCost = shippingTtotalCost ? shippingTtotalCost.textContent.trim() : '';
-            var productNameElements = document.getElementsByClassName('line-item-name');
+            var grandTotalPriceElement =
+                document.querySelector('.grand-total-price');
+            var orderDiscountPrice = document.querySelector(
+                '.order-discount-total'
+            );
+            var shippingTtotalCost = document.querySelector(
+                '.shipping-total-cost'
+            );
+            var grandTotalPrice = grandTotalPriceElement
+                ? grandTotalPriceElement.textContent.trim()
+                : '';
+            var orderDiscount = orderDiscountPrice
+                ? orderDiscountPrice.textContent.trim()
+                : '';
+            var shippingCost = shippingTtotalCost
+                ? shippingTtotalCost.textContent.trim()
+                : '';
+            var productNameElements =
+                document.getElementsByClassName('line-item-name');
             var productNames = [];
             for (var i = 0; i < productNameElements.length; i++) {
-                var productName = productNameElements[i].querySelector('span').textContent.trim();
+                var productName = productNameElements[i]
+                    .querySelector('span')
+                    .textContent.trim();
                 productNames.push(productName);
             }
             var purchaseData = {
                 totalPrice: grandTotalPrice,
-                discount:orderDiscount,
-                shippingPrice:shippingCost,
-                products:productNames
-
+                discount: orderDiscount,
+                shippingPrice: shippingCost,
+                products: productNames
             };
 
             dataLayer.push({
-                'event': 'make_purchase',
-                'grandTotalPrice': purchaseData.totalPrice,
-                'orderDiscountPrice':purchaseData.discount,
-                'shippingCost':purchaseData.shippingPrice,
-                'productNames':purchaseData.products
-
+                event: 'make_purchase',
+                grandTotalPrice: purchaseData.totalPrice,
+                orderDiscountPrice: purchaseData.discount,
+                shippingCost: purchaseData.shippingPrice,
+                productNames: purchaseData.products
             });
         });
     });
 
+    var addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+    addToCartButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var productNameElements =
+                document.getElementsByClassName('product-name');
+            var productName =
+                productNameElements.length > 0
+                    ? productNameElements[0].textContent.trim()
+                    : '';
+
+            var priceElement = document.querySelector('.prices');
+            var productPriceText = priceElement
+                ? priceElement.textContent.trim()
+                : '';
+            var regex = /£[\d,]+\.\d{2}/;
+            var match = productPriceText.match(regex);
+            var productPrice = match ? match[0] : '';
+
+            var productId = button.getAttribute('data-pid');
+
+            var product = {
+                name: productName,
+                id: productId,
+                price: productPrice
+            };
+
+            dataLayer.push({
+                event: 'add_to_cart',
+                Productname: product.name,
+                productid: product.id,
+                price: product.price
+            });
+        });
+    });
 });

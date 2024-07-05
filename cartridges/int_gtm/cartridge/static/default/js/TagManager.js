97ef3ac56a4563d47f5f6ee16a38c91b6f8173a6
MikeWheeler007 <arjun.bharath@g10x.com>
2024-07-05 14:06:34 +0530

    apply-promotion-fix

--- cartridges/int_gtm/cartridge/static/default/js/TagManager.js
+++ cartridges/int_gtm/cartridge/static/default/js/TagManager.js
@@ -1,14 +1,15 @@
 window.dataLayer = window.dataLayer || [];
-
+ 
 document.addEventListener('DOMContentLoaded', function () {
+   
     var promoForm = document.querySelector('.promo-code-form');
-
+ 
     promoForm.addEventListener('submit', function (event) {
         event.preventDefault();
-
+ 
         var couponCodeField = document.querySelector('.coupon-code-field');
         var couponCode = couponCodeField.value;
-
+ 
         if (couponCode.trim() === '') {
             document.getElementById('missingCouponCode').style.display =
                 'block';
@@ -16,10 +17,11 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             document.getElementById('missingCouponCode').style.display = 'none';
         }
-
+ 
         dataLayer.push({
             event: 'couponcheck',
             couponid: couponCode
         });
     });
+   
 });
